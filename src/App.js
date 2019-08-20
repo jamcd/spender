23e8530ce38af21fde1419b39476c63133ca12f5
Jamie McDonald <jamiemcd13@gmail.com>
2019-08-20 19:54:50 +0100

    Implemented simple server and axios request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+function Test() {
+  const [text, setText] = React.useState('')
+
+  React.useEffect(() => {
+    axios.get('/ping')
+    .then(response => {
+      setText(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  return (
+    <p>{text || 'Loading...'}</p>
+  )
+}
 
 function App() {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
