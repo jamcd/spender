30b3a70bcd9715544c7d35e38a74529dbbb3f455
Jamie McDonald <jamiemcd13@gmail.com>
2019-08-23 00:21:00 +0100

    Extracted Monzo login button into component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
+import MonzoLogin from './components/MonzoLogin'
 
 function Test() {
   const [text, setText] = React.useState('')
@@ -25,20 +25,7 @@ function App() {
   return (
     <div className="App">
       <Test />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MonzoLogin />
     </div>
   );
 }
