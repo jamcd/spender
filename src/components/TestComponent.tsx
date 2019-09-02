c83f4f4072c83c783e98cecdf5ea62d861e2ed8d
Jamie McDonald <jamiemcd13@gmail.com>
2019-09-02 17:40:14 +0100

    Fix incorrect formatting

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 
 const TestComponent: React.FC = () => {
-    const [text, setText] = React.useState("");
-  
-    React.useEffect(() => {
-      axios
-        .get("/ping")
-        .then(response => {
-          setText(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, []);
-  
-    return <p>{text || "Loading..."}</p>;
-  };
+  const [text, setText] = React.useState('');
 
-  export default TestComponent
+  React.useEffect(() => {
+    axios
+      .get('/ping')
+      .then(response => {
+        setText(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return <p>{text || 'Loading...'}</p>;
+};
+
+export default TestComponent;
