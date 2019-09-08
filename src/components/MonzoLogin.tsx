0af8336d120c45f73aecbe606b1311454a1d0ea6
Jamie McDonald <jamiemcd13@gmail.com>
2019-09-08 10:50:41 +0100

    Remove user's access token when landing on the login page

--- src/components/MonzoLogin.tsx
+++ src/components/MonzoLogin.tsx
@@ -5,10 +5,16 @@ import userOauthDetails from '../userOauthDetails';
 // redirects back to the app it will send a 'state' value back which needs to match the value that we originally
 // supplied. This will need to be stored in a DB or other persistent layer.
 
+// TODO: Handle security better. Either store creds on the server instead of client, or find another solution
+
 const MonzoLogin = () => {
   const { clientId } = userOauthDetails;
   const redirectUri = 'http://localhost:3000/oauth/callback';
   const monzoAuthUrl = 'https://auth.monzo.com';
+
+  React.useEffect(() => {
+    localStorage.removeItem('accessToken');
+  });
 
   return (
     <form action={monzoAuthUrl}>
