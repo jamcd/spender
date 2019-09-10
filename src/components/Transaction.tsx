f544d0a784e427efba4f3d8fa24c9a66ffc6d52d
Jamie McDonald <jamiemcd13@gmail.com>
2019-09-10 17:36:30 +0100

    Fixed layout of transactions

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Transaction = (props: any) => {
   return (
     <div className="transaction d-flex justify-content-between">
-      <div className="transaction__name align-self-start">
+      <div className="transaction__name">
         <p className="transaction__title">{props.transaction.merchant.name}</p>
         {props.transaction.notes && <p className="transaction__notes">{props.transaction.notes}</p>}
       </div>
-      <div className="transaction__cost align-self-end">
+      <div className="transaction__cost">
         <span>£{(Math.abs(props.transaction.amount) / 100).toFixed(0)}.</span>
         <sub>
           {(Math.abs(props.transaction.amount) / 100) % 1 === 0
