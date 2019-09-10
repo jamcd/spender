43800766998811a4f08b9d8406bf74a998dcf7d1
Jamie McDonald <jamiemcd13@gmail.com>
2019-09-10 17:46:03 +0100

    Added merchant logo to transactions

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 const Transaction = (props: any) => {
   return (
     <div className="transaction d-flex justify-content-between">
-      <div className="transaction__name">
-        <p className="transaction__title">{props.transaction.merchant.name}</p>
-        {props.transaction.notes && <p className="transaction__notes">{props.transaction.notes}</p>}
+      <div className="transaction__name d-flex">
+        {props.transaction.merchant && props.transaction.merchant.logo && (
+          <img src={props.transaction.merchant.logo} alt="logo" width="30" height="30" />
+        )}
+        <div className="d-flex flex-column ml-3">
+          <p className="transaction__title">{props.transaction.merchant.name}</p>
+          {props.transaction.notes && <p className="transaction__notes">{props.transaction.notes}</p>}
+        </div>
       </div>
       <div className="transaction__cost">
         <span>£{(Math.abs(props.transaction.amount) / 100).toFixed(0)}.</span>
