6d861b19d37b20ba3309dcaf7e4b3162d2abceab
Jamie McDonald <jamiemcd13@gmail.com>
2019-09-10 17:33:16 +0100

    Added notes to transactions

--- src/components/Transaction.tsx
+++ src/components/Transaction.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 const Transaction = (props: any) => {
   return (
     <div className="transaction d-flex justify-content-between">
-      <span className="align-self-start">{props.transaction.merchant.name}</span>
-      <span className="transaction__cost align-self-end">
+      <div className="transaction__name align-self-start">
+        <p className="transaction__title">{props.transaction.merchant.name}</p>
+        {props.transaction.notes && <p className="transaction__notes">{props.transaction.notes}</p>}
+      </div>
+      <div className="transaction__cost align-self-end">
         <span>£{(Math.abs(props.transaction.amount) / 100).toFixed(0)}.</span>
         <sub>
           {(Math.abs(props.transaction.amount) / 100) % 1 === 0
             ? '00'
             : (Number(Number((Math.abs(props.transaction.amount) / 100) % 1).toFixed(2)) * 100).toFixed(0)}
         </sub>
-      </span>
+      </div>
     </div>
   );
 };
